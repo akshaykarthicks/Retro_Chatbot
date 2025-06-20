9661f41457879eb71b5ce70ecc42a2b63521a335
AKSHAY KARTHICK <112257650+akshaykarthicks@users.noreply.github.com>
2025-06-20 20:45:15 +0530

    first commit

---
+++ chatapp/views.py
@@ -0,0 +1,68 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import auth
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+import openai
+
+from django.utils import timezone
+
+from .models import Chat
+
+
+openai.api_key = "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"
+openai.api_base = "https://openrouter.ai/api/v1"
+
+def ask_openai(question):
+    response = openai.ChatCompletion.create(
+        model="openai/gpt-3.5-turbo",  # or any other model available on OpenRouter
+        messages=[{"role": "user", "content": question}]
+    )
+    answer = response.choices[0].message.content
+    return answer
+# @login_required
+def index(request):
+    if request.method == 'POST':
+        message = request.POST.get('message')
+        response = ask_openai(message)
+        return JsonResponse({'response': response})
+    return render(request,'chatbot.html')
+@login_required
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'Invalid username or password'
+            return render(request, 'login.html', {'error_message': error_message})
+    else:
+        return render(request, 'login.html')
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 == password2:
+            if User.objects.filter(username=username).exists():
+                error_message = 'Username already exists'
+                return render(request, 'register.html', {'error_message': error_message})
+            try:
+                user = User.objects.create_user(username, email, password1)
+                user.save()
+                auth.login(request, user)
+                return redirect('index')
+            except:
+                error_message = 'Error creating account'
+                return render(request, 'register.html', {'error_message': error_message})
+        else:
+            error_message = 'Password dont match'
+            return render(request, 'register.html', {'error_message': error_message})
+    return render(request, 'register.html')
